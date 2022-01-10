b8c250d0fcacce00ac24c7b948e152e34218733d
sir William <williamfr0615@gmail.com>
2022-01-09 22:08:02 -0400

    added Components ...
    Folder Components
      Class Life Generator
      Class Life Itself
      Class World

---
+++ ConwaysGameOfLife/Components/World.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    class World
+    {
+        private struct Map 
+        {
+            public int[,] map { get; set; }
+
+            public Map(int size)
+            {
+                map = new int[size, size];
+            }
+        }
+
+        private Map activeWorld;
+        private Map newWorld;
+        private Map border;
+
+        public World()
+        {
+            int inputSize = 10; // MN#10: debug, changeto read from input.
+
+            activeWorld = new Map(inputSize);
+            newWorld = new Map(inputSize);
+            border = new Map(inputSize+2);
+        }
+
+        public void CreateBorder()
+        { 
+            
+        }
+
+        public void GenerateLife()
+        { 
+            
+        }
+
+        public void LiveLife()
+        { 
+        
+        }
+    }
+}
---
+++ ConwaysGameOfLife/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    class Program
+    {
+        //private fields
+        private LifeGenerator lifeGenerator;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Begin Conways Game Of Life\n\nCoded by William Fraser");
+
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}

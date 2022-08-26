8aa87ccbb2d26292821238d579a2def46e4feda4
William-Fraser <70350291+William-Fraser@users.noreply.github.com>
2022-08-25 23:41:18 -0300

    [added] World Generation

--- ConwaysGameOfLife/Components/World.cs
+++ ConwaysGameOfLife/Components/World.cs
@@ -10,56 +10,72 @@ namespace ConwaysGameOfLife
     {
         private struct Map 
         {
-            public char[,] map { get; set; }
+            public bool[,] map { get; set; }
 
             public Map(int size)
             {
-                map = new char[size, size];
+                map = new bool[size, size];
             }
         }
 
-        private Map activeWorld;
-        private Map newWorld;
-        private Map border;
+        private Map activeWorld; // currently displayed
+        private Map newWorld; // made from active
+        private Map border; // border looks nice
 
-        public World()
+        public World(int inputSize, int chanceOfLife)
         {
-            int inputSize = 10; // MN#10: debug, changeto read from input.
-
             activeWorld = new Map(inputSize);
             newWorld = new Map(inputSize);
             border = new Map(inputSize+2); // MN#+2 to add borders on all sides
+
+            CreateActive(inputSize, chanceOfLife);
+            CreateBorder();
         }
 
-        public void CreateBorder()
+        public void CheckAndSetActive()
         {
-            for (int y = 0; y == border.map.GetLength(0); y++) 
+            if (newWorld.map != activeWorld.map)
             {
-                for (int x = 0; x == border.map.GetLength(0); x++) // the length is the same for both indexers
+                activeWorld = newWorld;
+            }
+        }
+
+        //create
+        private void CreateBorder() // creates a border to be displayed around the world
+        {
+            int borderLengthY = border.map.GetLength(0);
+            int borderLengthX = border.map.GetLength(1);
+
+            for (int y = 0; y == borderLengthY; y++) 
+            {
+                for (int x = 0; x == borderLengthX; x++)
                 { 
-                    if (y == 0 || y == border.map.GetLength(0) || x == 0 || x == border.map.GetLength(1))
+                    if (y == 0 || y == borderLengthY || x == 0 || x == borderLengthX) // only find border pieces on the border
                     {
-                        if (y == 0 || y == border.map.GetLength(0))
+                        //sides
+                        if (y == 0 || y == borderLengthY)
                         {
                             border.map[y, x] = '═';
                         }
-                        else if (x == 0 || x == border.map.GetLength(0))
+                        else if (x == 0 || x == borderLengthX)
                         {
                             border.map[y, x] = '║';
                         }
+
+                        //corners
                         else if (y == 0 || x == 0)
                         {
                             border.map[y, x] = '╔';
                         }
-                        else if (y == 0 || x == border.map.GetLength(0))
+                        else if (y == 0 || x == borderLengthX)
                         {
                             border.map[y, x] = '╗';
                         }
-                        else if (y == border.map.GetLength(0) || x == 0)
+                        else if (y == borderLengthY || x == 0)
                         {
                             border.map[y, x] = '╚';
                         }
-                        else if (y == border.map.GetLength(0) || x == border.map.GetLength(0))
+                        else if (y == borderLengthY || x == borderLengthX)
                         {
                             border.map[y, x] = '╝';
                         }
@@ -67,13 +83,56 @@ namespace ConwaysGameOfLife
                 }
             }
         }
+        private void CreateActive(int inputSize, int chanceOfLife) // create active world with life generator
+        {
+            LifeGenerator lifeGen = new LifeGenerator();
 
-        public void GenerateLife() // create active world
+            int activeWorldLengthY = activeWorld.map.GetLength(0);
+            int activeWorldLengthX = activeWorld.map.GetLength(1);
+
+            for (int y = 0; y == activeWorldLengthY; y++)
+            {
+                for (int x = 0; x == activeWorldLengthX; x++)
+                {
+                    activeWorld.map[y, x] = lifeGen.CreateLife(inputSize, chanceOfLife);
+                }
+            }
+        }
+
+        //draw
+        private void DrawActive() // display active world
+        {
+            int activeWorldLengthY = activeWorld.map.GetLength(0);
+            int activeWorldLengthX = activeWorld.map.GetLength(1);
+
+            for (int y = 0; y == activeWorldLengthY; y++)
+            {
+                for (int x = 0; x == activeWorldLengthX; x++)
+                {
+                    Console.Write(activeWorld.map[y, x]);
+                }
+            }
+        }
+        private void DrawBorder() // draw border around world
+        {
+            int borderLengthY = activeWorld.map.GetLength(0);
+            int borderLengthX = activeWorld.map.GetLength(1);
+
+            for (int y = 0; y == borderLengthY; y++)
+            {
+                for (int x = 0; x == borderLengthX; x++)
+                {
+                    Console.Write(border.map[y, x]);
+                }
+            }
+        }
+        
+        private void BufferNew() // moderate new world from active world using the rules of life itself
         { 
             
         }
 
-        public void LiveLife() // creates new world from active world using the rules of life itself
+        public void Update()
         { 
         
         }
